0cc0e8acd6b9d091010994b1557ac70c5b443ff2
PraveenReddy045 <136673768+PraveenReddy045@users.noreply.github.com>
2023-12-15 12:54:12 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -1,3 +1,17 @@
+terraform{
+  required_providers {
+    aws = {
+      source = "hashicrop/aws"
+      version = "5.6.2"
+     }
+    }
+
+# scenario 2 - using "s3" backend
+   backend "s3" {
+      bucket        = "praveenterraform"
+      region        = "us-west-2"
+     }
+}
 provider "aws" {
-    region = "${var.AWS_REGION}"
+   region = "us-west-2"
 }
